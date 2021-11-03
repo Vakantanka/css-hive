919416973af95f57991d1ed9007e7bd75b25a066
Vakantanka <laszlo.fenyvesi@gmail.com>
2021-11-03 20:46:12 +0100

    A 17.ik találattól határozottan idegesítő lesz

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function loadEvent() {
-	console.log("Load completed.");
-	const catchMe = document.getElementById("catchMe");
 	let counter = 1;
+	let duration = 10;
+	
+	const catchMe = document.getElementById("catchMe");
+	catchMe.style.setProperty('animation-duration', duration +'s');
+	catchMe.style.setProperty('top', 0 + "%");
+	catchMe.style.setProperty('left', 0 +'%');
+
 	catchMe.addEventListener("click", function() {
-		alert("Nice! " + counter + " click(s)");
+		let score = document.getElementById("score");
+		let top = Math.floor(Math.random() * 100);
+		let left = Math.floor(Math.random() * 100);
+		
+		score.innerText= counter;
 		counter++;
-	})
+		if (counter % 2 === 0) {
+			if (duration > 0) {
+				duration--;
+				catchMe.style.setProperty('animation-duration', duration +'s');
+			}
+		}
+		catchMe.style.setProperty('top', top +'%');
+		catchMe.style.setProperty('left', left +'%');
+	});
 }
 
 window.addEventListener("load", loadEvent);
